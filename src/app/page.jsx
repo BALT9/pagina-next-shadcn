91dc5e04be803119bdab6b68d60e7ab6f9b9b3e0
willy <willybalt.2017@gmail.com>
2025-07-27 11:43:55 -0400

    configuracion da tailwind

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home page</h1>
+      <h2>lorem</h2>
+      <Button>Hola mundo</Button>
+    </>
+  );
+}
